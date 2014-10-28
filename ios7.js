269bacd34533abd8c7d46e8b6da172f9afddfe9f
Adam D <kirbylover4000@gmail.com>
2014-10-28 21:06:01 +1030

    don't explode if there's no middot in the page title

--- ios7.js
+++ ios7.js
@@ -6,7 +6,10 @@
 	var classList = document.documentElement.classList;
 
 	if (navigator.userAgent.indexOf("Cydia") != -1) {
-		document.title = doc.title.split(" \u00b7 ")[0];
+		if (document.title.indexOf(" \u00b7 ") != -1) {
+			document.title = document.title.split(" \u00b7 ")[0];
+		}
+
 		classList.add("cydia");
 	} else {
 		classList.remove("cydia", "depiction");
