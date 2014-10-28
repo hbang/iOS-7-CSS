63bcd8e7e3021cce6854f50d05c4f71acc01e94c
Adam D <kirbylover4000@gmail.com>
2014-10-28 21:00:13 +1030

    make the javascript ever so slightly smaller
    SAVE ALL THE BYTES

--- ios7.js
+++ ios7.js
@@ -1,10 +1,14 @@
 /* Basic iOS 7 CSS // Apache 2 license // hbang.ws */
 
-(function(doc) {
+(function(document) {
+	"use strict";
+
+	var classList = document.documentElement.classList;
+
 	if (navigator.userAgent.indexOf("Cydia") != -1) {
-		doc.title = doc.title.split(" \u00b7 ")[0];
-		doc.documentElement.classList.add("cydia");
+		document.title = doc.title.split(" \u00b7 ")[0];
+		classList.add("cydia");
 	} else {
-		doc.documentElement.classList.remove("cydia", "depiction");
+		classList.remove("cydia", "depiction");
 	}
 })(document);
